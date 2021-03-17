77d61c0ca007af1733a944d446aa191f2626375b
Dima Zubkov <dima5447298@gmail.com>
2021-03-18 00:41:56 +0300

    netlify ругается на blockType=TMP

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,3 @@
-<TMP>
-  <div>
-    <div class="max-w-sm rounded overflow-hidden shadow-lg mb-4">
-      <img
-        class="w-full"
-        src="https://tailwindcss.com/img/card-top.jpg"
-        alt="Sunset in the mountains"
-      />
-      <div class="px-6 py-4 pb-2">
-        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-        <p class="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-          nihil.
-        </p>
-      </div>
-      <div class="px-6 py-4">
-        <span class="badge mr-2">#photography</span>
-        <span class="badge mr-2">#travel</span>
-        <span class="badge">#winter</span>
-      </div>
-    </div>
-
-    <h3 class="text-3xl mt-2">Tailwind config:</h3>
-    <pre class="bg-gray-200 p-3 rounded">{{ tailwindConfig }}</pre>
-  </div>
-</TMP>
-
 <template lang="pug">
 div
   .max-w-sm.rounded.overflow-hidden.shadow-lg.mb-4
